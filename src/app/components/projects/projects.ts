1605da3066feeff38383e67a073ac2d57e457caa
PANKUI KAMTCHA MESMINE CHANELLE <mesminekamtcha@gmail.com>
2026-08-31 12:51:02 +0100

    lighthouse

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { PROJECTS } from '../../data/projects';
 import { ICONS } from '../../shared/icons';
 import { getTechIcon } from '../../shared/tech-icons';
+import { SafeHtmlPipe } from '../../shared/safe-html.pipe';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
+  imports: [SafeHtmlPipe],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
